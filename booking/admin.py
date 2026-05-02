ccc5fa7931393c6766a1181d3705ab9bb89b3e74
kuwajimayutaka-a11y <kuwajima.yutaka@gmail.com>
2026-05-03 02:11:59 +0900

    Django admin修正: CustomUser用フォームを設定してusername参照エラーを解消

--- booking/admin.py
+++ booking/admin.py
@@ -1,10 +1,44 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
 from .models import CustomUser, Family, Student, LessonSlot, Reservation, Waitlist
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+        fields = ('email', 'name')
+
+
+class CustomUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='パスワード', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='パスワード（確認）', widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'name')
+
+    def clean_password2(self):
+        p1 = self.cleaned_data.get('password1')
+        p2 = self.cleaned_data.get('password2')
+        if p1 and p2 and p1 != p2:
+            raise forms.ValidationError('パスワードが一致しません')
+        return p2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        user.is_active = True
+        if commit:
+            user.save()
+        return user
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
     model = CustomUser
     list_display = ('email', 'name', 'is_staff', 'is_active', 'date_joined')
     list_filter = ('is_staff', 'is_active', 'is_superuser')
@@ -22,7 +56,6 @@ class CustomUserAdmin(UserAdmin):
     )
     search_fields = ('email', 'name')
     ordering = ('email',)
-    # username フィールドが存在しないため親クラスの参照を無効化
     filter_horizontal = ('groups', 'user_permissions')
 
 
